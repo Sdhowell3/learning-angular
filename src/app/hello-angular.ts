1cddeb1892ff6ac652b43b9cc3046822f0e66c32
Nathaniel Inman <nate@theoestudio.com>
2017-09-18 09:58:44 -0500

    converting gulp to webpack.

--- src/app/hello-angular.ts
+++ src/app/hello-angular.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 @Component({
-  selector: 'hello-angular',
+  selector: 'app',
   template: '<h1> {{greeting}} </h1>'
 })
 
